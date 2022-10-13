587e3460308a820e50819b6f69731c1b20b7a4c5
BorrachaFox <daviantoniosantos@outlook.com>
2022-10-13 13:00:47 -0300

    Initial commit

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Envelope } from 'phosphor-react'
+import { TextInput, TextInputInputProps, TextInputRootProps } from './TextInput'
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon> 
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder='Type your amail adress'/>
+    ],
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+  }
+} as Meta<TextInputInputProps>
+
+export const Default: StoryObj<TextInputInputProps> = {}
+
+export const WithoutIcon: StoryObj<TextInputInputProps> = {
+  args: {
+    children: <TextInput.Input placeholder='Type your amail adress'/>
+  }
+}
